5322af23356995c9181ba3b365b0ba37d23f9b3b
root <31539681+bhatRV@users.noreply.github.com>
2019-04-10 11:52:57 +0530

    Update Application.java

--- src/main/java/rv/retail/Application.java
+++ src/main/java/rv/retail/Application.java
@@ -3,9 +3,11 @@ package rv.retail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"rv.retail"})
+@EnableSwagger2
 
 public class Application {
 
